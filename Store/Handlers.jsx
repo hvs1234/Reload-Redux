5a19d75f58bc18293eaab2f6da076e244f277fb4
Harshvardhan Sharma <102682161+hvs1234@users.noreply.github.com>
2024-08-02 10:56:57 +0530

    Add files via upload

---
+++ Store/Handlers.jsx
@@ -0,0 +1,21 @@
+import { useDispatch } from "react-redux";
+import { setPath } from "./Slice";
+import { useCallback } from "react";
+
+const Handlers = () => {
+  const dispatch = useDispatch();
+  //   const open = useSelector((state) => state.app.open);
+
+  const handleOnClick = useCallback(
+    (path) => (e) => {
+      e.preventDefault();
+      dispatch(setPath(path));
+      window.location.pathname = path;
+    },
+    [dispatch]
+  );
+
+  return { handleOnClick };
+};
+
+export default Handlers;
